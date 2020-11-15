967c8cd2a4a27735f5a0258011a3dd5143dc3f3a
ofanan <ofanan@gmail.com>
2020-11-15 17:09:45 +0200

    a
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/LP_file_parser.py

--- src/LP_file_parser.py
+++ src/LP_file_parser.py
@@ -1,17 +1,32 @@
 import numpy as np
 import itertools 
-
+import re
 
 class LP_file_parser (object):
+    
+    
+    def parse_lin_comb (self, lin_comb_string):
+        filtered = filter(None, re.split("[, \-+]+", lin_comb_string))
+        for item in filtered:
+            print (item)
+        
+#         for item in (fiter (None, lin_comb_string.split(" [,+-]")):
+#             print (item)
     
     
     def parse_line (self, line):
         splitted_line = line.split (" ")
          
         if (splitted_line[0] == 'subject' and splitted_line[1] == 'to'):
-            print ('gamad') 
+            constraint_string_splitted = line.split(":")[1].split("=")
+            constraint_string_split_lt = constraint_string_splitted[0].split("<")
+            op = '='
+            if (len (constraint_string_split_lt)==2):                
+                op = '<='
+                self.parse_lin_comb(constraint_string_split_lt[0])
+                print ('******')
         elif (splitted_line[0] == 'minimize' and splitted_line[1] == 'z:'):
-            print ('nanas')
+            objective_func_string = line.split(":")[1]
 
     
     def parse_LP_file (self, input_file_name):
@@ -20,6 +35,7 @@ class LP_file_parser (object):
         # self.output_file = open ("../res/" + input_file_name.split(".")[0] + ".dat", "a")  
         lines = (line.rstrip() for line in self.input_file) # "lines" contains all lines in input file
         lines = (line for line in lines if line)       # Discard blank lines
+
         self.cost_func   = []
         self.constraints = []
     

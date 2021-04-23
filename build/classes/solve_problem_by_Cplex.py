95559a01884290a32daf8e19732c8e2a740a1fab
ofanan <ofanan@gmail.com>
2021-04-23 17:22:39 +0300

    unified Java (cityMob) and Python (gen_network, CPlex) under a single src directory.
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/.gitignore
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/Check_sol.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/LP_file_parser.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/__pycache__/Check_sol.cpython-37.pyc
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/__pycache__/obj_func.cpython-37.pyc
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/__pycache__/printf.cpython-37.pyc
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/cityMob_output_parser.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/gen_LP_cplex.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/gen_network.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/gen_network_w_LP.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/obj_func.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/printf.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/solve_problem_by_Cplex.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/build/classes/toy_example.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/LP_file_parser.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/cityMob_output_parser.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/gen_LP_cplex.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/gen_network.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/gen_network_w_LP.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/printf.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/solve_problem_by_Cplex.py
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/toy_example.py

---
+++ build/classes/solve_problem_by_Cplex.py
@@ -0,0 +1,45 @@
+import sys
+import numpy as np
+import cplex
+from doopl.factory import *
+import docplex
+from printf import printf
+
+
+
+def solve_problem_by_Cplex (input_file_name):
+    """
+    Accept as input a file containing a LP problem, at an .lp format, and the number of decision variables.
+    Solves the problem using Cplex, and then prints the sol's status, the obj's func value, and the decision var' vals.
+    """
+
+    output_file = open ('../res/' + input_file_name.split(".lp")[0] + '.cpx.sol', "w")
+    problem = cplex.Cplex (input_file_name) 
+    problem.solve () #TimeLimit=1000?
+
+    
+    sol     = problem.solution
+    print  ('\n***********************\nSol status: {}\n***********************\n' .format (sol.get_status_string()))
+    sol_status_string = sol.get_status_string()
+    printf (output_file, 'Sol status: {}\n' .format (sol_status_string))
+    if (sol_status_string == 'infeasible'):
+        return None
+    
+    printf (output_file, 'Solution value  = {:.2f}\n' .format (sol.get_objective_value()))
+    print  (             'lp sol cost = {:.2f}\n' .format (sol.get_objective_value()))
+    printf (output_file, '\nList of non-zeros decision variables\n************************************\n' .format (sol.get_objective_value()))
+    list_of_set_vars = []
+    for var in problem.variables.get_names():
+        var_val = sol.get_values (var)
+        if (var_val == 0):
+            continue
+        if (var_val != 1):
+            print ('Warning: Non-boolean value in the solution: {} = {}' .format (var, var_val))
+        printf (output_file, '{} = {:.2f}, ' .format (var, sol.get_values (var)))
+        list_of_set_vars.append ( int(var.split('x')[1]) )
+    printf (output_file, '\n')
+    return list_of_set_vars
+
+if __name__ == "__main__":
+    solve_problem_by_Cplex ('../res/problem.lp')
+

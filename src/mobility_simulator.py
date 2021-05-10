9554c4380b60e96a4e10587518864a7031a02f5d
ofanan <ofanan@gmail.com>
2021-05-10 21:02:20 +0300

    Added an exact update of the location (and not only AP) upon each event. Not debugged yet.
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/mobility_simulator.py

--- src/mobility_simulator.py
+++ src/mobility_simulator.py
@@ -45,9 +45,12 @@ class my_mobility_simulator (object):
     """
 
     # Calculate the arrival time, given the current location, the destination and the speed.
-    arr_t     = lambda self, cur_loc, dst, speed : self.cur_time + math.sqrt (pow(cur_loc[X] - dst[X], 2) + pow(cur_loc[Y] - dst[Y], 2)) / speed
+    arr_t      = lambda self, cur_loc, dst, speed : self.cur_time + math.sqrt (pow(cur_loc[X] - dst[X], 2) + pow(cur_loc[Y] - dst[Y], 2)) / speed
     # Calculate which AP covers a concrete point. The input, "loc", is a vector, containing the X,Y locations.
-    loc2ap    = lambda self, loc : int (math.floor ((loc[Y] / self.cell_edge) ) * self.num_of_APs_in_row + math.floor ((loc[X] / self.cell_edge) )) 
+    loc2ap     = lambda self, loc : int (math.floor ((loc[Y] / self.cell_edge) ) * self.num_of_APs_in_row + math.floor ((loc[X] / self.cell_edge) )) 
+    # Update the location of a usr. New location is calculated as: 
+    # last_updated_location + time_since_last_location_update * directional_speed, for each dimension (that is, X and Y).
+    update_loc = lambda self, usr : usr['cur loc'] + (self.cur_time - self['last update time']) * usr['speed']
       
     def start_mov_usr (self, usr):
         """
@@ -58,19 +61,27 @@ class my_mobility_simulator (object):
         """
 
         usr['final loc'] = self.edge * np.random.rand (2)  # destination of movement
-        if (any ([usr['final loc'][i]>self.edge for i in [0,1]])):
-            print (usr['final loc'])
-            exit ()
-        usr['final ap' ] = self.loc2ap (usr['final loc'])
+        usr['final ap'        ] = self.loc2ap (usr['final loc'])
+        self.calc_usr_speed(usr)
         self.eventQ.append ({'event type'   : EVENT_T_ARRIVE,
                              'time'         : self.arr_t (usr['cur loc'], usr['final loc'], usr['max speed']),
                              'usr'          : usr
                              })
         if (usr['final ap' ] == usr['cur ap']): # the move is within the coverage of the current AP - no need to schedule additional events 
             return
+        self.mov_usr (usr)
+
+    def calc_usr_speed (self, usr): 
+        """
+        Calculate the projectory of the speed of a given usr in each direction, as follows:
+        - pick a speed u.a.r. from [usr['min speed'], usr['max speed']].
+        - calculate the projection of the speed on the X, Y axes; take into the account (speed[x] is negative when the direction is to smaller x; same with speed[y]). 
+        - Write the speed to usr['speed']  
+        """
+        speed = usr['min speed'] + np.random.rand () * (usr['max speed'] - usr['min speed']) 
         theta = np.arctan (abs( (usr['final loc'][Y] - usr['cur loc'][Y]) / (usr['final loc'][X] - usr['cur loc'][X]) )) # angle of progression
         usr['speed'] = np.array ([math.cos(theta)*usr['max speed'], math.sin(theta)*usr['max speed']])  #Projection of the speed on the X, Y, direction
-        self.mov_usr (usr)
+        usr['speed'] = np.array([usr['speed'][dim] if (usr['final loc'][dim] > usr['cur loc'][dim]) else -usr['speed'][dim] for dim in [X,Y]])        
 
     def mov_usr (self, usr):        
         time_to_mig = np.ones(2) * np.inf
@@ -78,9 +89,9 @@ class my_mobility_simulator (object):
             lower_borderline = self.cell_edge * math.floor(usr['cur loc'][dim] / self.cell_edge)
             offset = usr['cur loc'][dim] % self.cell_edge # offset of the usr's current location within its current cell
             if (usr['final loc'][dim] < lower_borderline):
-                time_to_mig[dim] = offset / usr['speed'][dim]
+                time_to_mig[dim] = offset / abs(usr['speed'][dim])
             elif (usr['final loc'][dim] > lower_borderline + self.cell_edge):
-                time_to_mig[dim] = (self.cell_edge - offset) / usr['speed'][dim]
+                time_to_mig[dim] = (self.cell_edge - offset) / abs(usr['speed'][dim])
 
         time_to_mig = min (time_to_mig)
         if (time_to_mig == np.inf): # the final destination is within the current cell - no need to schedule a future mig' event
@@ -89,8 +100,6 @@ class my_mobility_simulator (object):
         # Deter fatal problem when the time to migration is 0 (happens when the user is exactly on the border between coverage areas) 
         time_to_mig = max (time_to_mig, EPSILON)
         
-        direction = [1 if (usr['final loc'][dim] > usr['cur loc'][dim]) else -1 for dim in [X,Y]]
-        usr['nxt loc'] = usr['cur loc'] + [time_to_mig*x*y for (x,y) in zip(usr['speed'], direction)]
         self.eventQ.append ({'event type'   : EVENT_T_MIG,
                              'time'         : self.cur_time + time_to_mig,
                              'usr'          : usr
@@ -110,6 +119,8 @@ class my_mobility_simulator (object):
         """
         printf (self.loc_output_file, 'time = {:.4f} : \n' .format (self.cur_time)) 
         for u in range(self.NUM_OF_USRS):
+            self.usr[u]['cur loc']          = self.update_loc (self.usr[u]) 
+            self.usr[u]['last update time'] = self.cur_time
             printf (self.loc_output_file, 'usr {} {:.1f} {:.1f}\n' .format (u, self.usr[u]['cur loc'][X], self.usr[u]['cur loc'][Y]))
         printf (self.loc_output_file, '\n')    
 
@@ -155,7 +166,9 @@ class my_mobility_simulator (object):
                                      'time'   : 0})                 
            
         if (self.verbose > 0): # If the verbose level requires that, write to file the initial APs of each user
-            self.print_APs()   
+            self.print_APs()
+            if (sim.print_locations):
+                self.print_locs()   
             
         # Start move the usrs   
         for u in range(self.NUM_OF_USRS):
@@ -171,20 +184,23 @@ class my_mobility_simulator (object):
                 self.cur_time = event['time']
         
                 if (event['event type'] == EVENT_T_MIG):
-                    usr             = event['usr']  
-                    usr ['cur loc'] = usr['nxt loc']
-                    usr ['cur ap']  = self.loc2ap (usr['cur loc'])
+                    usr = event['usr']  
+                    usr ['cur loc'         ] = self.update_loc (usr)
+                    usr ['cur ap'          ] = self.loc2ap (usr['cur loc'])
+                    usr ['last update time'] = self.cur_time
                     if (self.verbose == 1 or self.verbose == 3):
                         self.print_APs ()
                     self.mov_usr (usr) 
                 elif (event['event type'] == EVENT_T_ARRIVE):
                     usr = event['usr']  
-                    usr ['cur loc'] = usr['final loc']
+                    usr ['cur loc'         ] = usr['final loc']
+                    usr ['last update time'] = self.cur_time
                     self.eventQ.append ({'event type' : EVENT_T_START_MOV,
                                          'time'       : self.cur_time + np.random.rand () * (usr['max stnd time'] - usr['min stnd time']),
                                          'usr'        : usr
                                          })
                 elif (event['event type'] == EVENT_T_START_MOV):
+                    usr['last update time'] = self.cur_time
                     self.start_mov_usr(event['usr'])
                 elif (event['event type'] == EVENT_T_PERIODICAL):
                     self.print_APs()
@@ -207,10 +223,11 @@ class my_mobility_simulator (object):
 
         self.usr = [{} for u in range (self.NUM_OF_USRS)]
         for u in range (self.NUM_OF_USRS):
-            self.usr[u] = {'max speed'      : (30 + 5*u)/ 3.6, 
-                           'cur loc'       : np.random.rand (2) * self.edge,
-                           'min stnd time' : 0.5,
-                           'max stnd time' : 1.5
+            self.usr[u] = {'min speed'      : 30 / 3.6,
+                           'max speed'      : 80 / 3.6,                            
+                           'min stnd time'  : 0.5,
+                           'max stnd time'  : 1.5,
+                           'cur loc'       : np.random.rand (2) * self.edge
                             }  # speed [m/sec]
             self.usr[u]['cur ap'] = self.loc2ap (self.usr[u]['cur loc'])
         
@@ -222,6 +239,6 @@ class my_mobility_simulator (object):
 
 if __name__ == "__main__":
     sim = my_mobility_simulator ()
-    # sim.print_locations = True # For printing also users' locations. Currently un-supported, because finding fresh accurate users' location requires complicated calculations. 
+    sim.print_locations = True # For printing also users' locations. Currently un-supported, because finding fresh accurate users' location requires complicated calculations. 
     sim.simulate (verbose = VERBOSE_EVENTS_AND_PERIODICAL)
         

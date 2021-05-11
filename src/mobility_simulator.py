3675542451545f2f95418141b59f85344a66fde3
ofanan <ofanan@gmail.com>
2021-05-11 11:32:29 +0300

    a
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/mobility_simulator.py

--- src/mobility_simulator.py
+++ src/mobility_simulator.py
@@ -46,8 +46,10 @@ class my_mobility_simulator (object):
 
     # Calculate the arrival time, given the current location, the destination and the speed.
     arr_t      = lambda self, cur_loc, dst, speed : self.cur_time + math.sqrt (pow(cur_loc[X] - dst[X], 2) + pow(cur_loc[Y] - dst[Y], 2)) / speed
+
     # Calculate which AP covers a concrete point. The input, "loc", is a vector, containing the X,Y locations.
     loc2ap     = lambda self, loc : int (math.floor ((loc[Y] / self.cell_edge) ) * self.num_of_APs_in_row + math.floor ((loc[X] / self.cell_edge) )) 
+    
     # Update the location of a usr. New location is calculated as: 
     # last_updated_location + time_since_last_location_update * directional_speed, for each dimension (that is, X and Y).
     update_loc = lambda self, usr : usr['cur loc'] + (self.cur_time - usr['last update time']) * usr['speed']
@@ -78,7 +80,7 @@ class my_mobility_simulator (object):
         - calculate the projection of the speed on the X, Y axes; take into the account (speed[x] is negative when the direction is to smaller x; same with speed[y]). 
         - Write the speed to usr['speed']  
         """
-        speed = usr['min speed'] + np.random.rand () * (usr['max speed'] - usr['min speed']) 
+        speed = random.uniform (usr['min speed'], usr['max speed']) 
         theta = np.arctan (abs( (usr['final loc'][Y] - usr['cur loc'][Y]) / (usr['final loc'][X] - usr['cur loc'][X]) )) # angle of progression
         usr['speed'] = np.array ([math.cos(theta)*usr['max speed'], math.sin(theta)*usr['max speed']])  #Projection of the speed on the X, Y, direction
         usr['speed'] = np.array([usr['speed'][dim] if (usr['final loc'][dim] > usr['cur loc'][dim]) else -usr['speed'][dim] for dim in [X,Y]])        
@@ -118,10 +120,16 @@ class my_mobility_simulator (object):
         Currently unused.
         """
         printf (self.loc_output_file, 'time = {:.4f} : \n' .format (self.cur_time)) 
-        for u in range(self.NUM_OF_USRS):
-            self.usr[u]['cur loc']          = self.update_loc (self.usr[u]) 
-            self.usr[u]['last update time'] = self.cur_time
-            printf (self.loc_output_file, 'usr {} {:.1f} {:.1f}\n' .format (u, self.usr[u]['cur loc'][X], self.usr[u]['cur loc'][Y]))
+
+        usr = self.usr[0]
+        usr['cur loc']          = self.update_loc (usr) 
+        usr['last update time'] = self.cur_time
+        printf (self.loc_output_file, 'usr {} {:.2f} {:.2f}\n' .format (usr['id'], usr['cur loc'][X], usr['cur loc'][Y]))
+
+        # for usr in self.usr[0]: 
+        #     usr['cur loc']          = self.update_loc (usr) 
+        #     usr['last update time'] = self.cur_time
+        #     printf (self.loc_output_file, 'usr {} {:.1f} {:.1f}\n' .format (usr['id'], usr['cur loc'][X], usr['cur loc'][Y]))
         printf (self.loc_output_file, '\n')    
 
     def print_APs (self):
@@ -137,7 +145,11 @@ class my_mobility_simulator (object):
         if (self.print_APs_flag):
             self.print_APs()
         if (self.print_locs_flag):
-            self.print_locs()          
+            self.print_locs()  
+        if (self.print_trajectory_of_usr >= 0): # and any ([self.usr[self.print_trajectory_of_usr]['speed'][dim] > 0 for dim in [X,Y]])):
+            printf (self.trajectory_output_file, '({:.2f}, {:.2f})' .format (self.usr[self.print_trajectory_of_usr]['cur loc'][X],
+                                                                             self.usr[self.print_trajectory_of_usr]['cur loc'][Y]
+                                                                             ))        
             
     def simulate (self, verbose):
         """
@@ -153,19 +165,22 @@ class my_mobility_simulator (object):
         
         if (self.verbose > 0):
             # Overwrite old output file, if exists
-            with open('../res/my_mob_sim.ap', 'w') as self.ap_output_file:
-                self.ap_output_file.write('')
-                
-            self.ap_output_file  = open ("../res/my_mob_sim.ap",  "w")
+            with open('../res/mob_sim.ap', 'w') as self.ap_output_file:
+                self.ap_output_file.write('')                
+            self.ap_output_file  = open ("../res/mob_sim.ap",  "w")
+            
             printf (self.ap_output_file,  '// File format:\n//time = t: (1,a1),(2,a2), ...\n//where aX is the Point-of-Access of usr X at time t\n\n')
 
             if (self.print_locs_flag):                 
-                self.loc_output_file = open ("../res/my_mob_sim.loc", "w")
+                self.loc_output_file = open ("../res/mob_sim.loc", "w")
                 printf (self.loc_output_file, '// mobility - output by mobility_simulator.py\n')
                 printf (self.loc_output_file, '// File format:\n//time = t:\n')
                 printf (self.loc_output_file, '// usr U (X, Y)\n//where (X,Y) is the location of usr U at time t\n')
                 printf (self.loc_output_file, '// the simulation is done in a rectangle of size MAX_X * MAX_Y, where\n')       
                 printf (self.loc_output_file, 'MAX_X {:.0f} MAX_Y {:.0f}\n\n' .format(self.edge, self.edge))
+                
+            if (self.print_trajectory_of_usr >= 0):
+                self.trajectory_output_file = open ("../res/mob_sim.trj", "w")
  
         if (self.verbose > 1): # If the verbose level requires that, begin a series of periodical events  
             self.eventQ.append ({'event type' : EVENT_T_PERIODICAL,
@@ -177,7 +192,7 @@ class my_mobility_simulator (object):
         # Start move the usrs   
         for usr in self.usr:
             self.eventQ.append ({'event type' : EVENT_T_START_MOV,
-                                 'time'       : np.random.rand () * (usr['max stnd time'] - usr['min stnd time']),
+                                 'time'       : random.uniform(usr['min stnd time'], usr['max stnd time']), 
                                  'usr'        : usr
                                  })
     
@@ -202,10 +217,13 @@ class my_mobility_simulator (object):
                     usr = event['usr']  
                     usr ['cur loc'         ] = usr['final loc']
                     usr ['last update time'] = self.cur_time
+                    usr ['speed'           ] = np.zeros (2)
                     self.eventQ.append ({'event type' : EVENT_T_START_MOV,
-                                         'time'       : self.cur_time + np.random.rand () * (usr['max stnd time'] - usr['min stnd time']),
+                                         'time'       : self.cur_time + random.uniform(usr['min stnd time'], usr['max stnd time']),
                                          'usr'        : usr
                                          })
+                    if (usr['id'] == self.print_trajectory_of_usr):
+                        printf (self.trajectory_output_file, '\n\n')
                 elif (event['event type'] == EVENT_T_START_MOV):
                     usr['last update time'] = self.cur_time
                     self.start_mov_usr(event['usr'])
@@ -230,7 +248,8 @@ class my_mobility_simulator (object):
 
         self.usr = [{} for u in range (self.NUM_OF_USRS)]
         for u in range (self.NUM_OF_USRS):
-            self.usr[u] = {'min speed'        : 30 / 3.6,
+            self.usr[u] = {'id'               : u,
+                           'min speed'        : 30 / 3.6,
                            'max speed'        : 80 / 3.6,                            
                            'min stnd time'    : 0.5,
                            'max stnd time'    : 1.5,
@@ -245,11 +264,12 @@ class my_mobility_simulator (object):
         self.cur_time = 0
         self.MAX_TIME = 20
 
-        self.T_BETWEEN_PERIODICAL_EVENTS = 1 # time between sequencing prints of all usrs' locations        
+        self.T_BETWEEN_PERIODICAL_EVENTS = 0.1 # time between sequencing prints of all usrs' locations        
 
 if __name__ == "__main__":
     sim = my_mobility_simulator ()
     sim.print_APs_flag  = True
-    sim.print_locs_flag = True # For printing also users' locations. Currently un-supported, because finding fresh accurate users' location requires complicated calculations. 
-    sim.simulate (verbose = VERBOSE_EVENTS_AND_PERIODICAL)
+    sim.print_locs_flag = True # For printing also users' locations. Currently un-supported, because finding fresh accurate users' location requires complicated calculations.
+    sim.print_trajectory_of_usr = 0 # Plot the trajectory of the requested user. For plotting no trajectories, assign -1.   
+    sim.simulate (verbose = VERBOSE_ONLY_PERIODICAL)
         

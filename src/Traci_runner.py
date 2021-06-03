ab3c8b0a042bb38980d41217d6552d72318adcde
ofanan <ofanan@gmail.com>
2021-06-03 11:52:43 +0300

    a
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/.gitignore
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/Traci_runner.py

--- src/Traci_runner.py
+++ src/Traci_runner.py
@@ -29,8 +29,10 @@ import random
 from sumolib import checkBinary  # noqa
 import traci  # noqa
 
-
 from printf import printf 
+
+# An accessory inline function, to filter out some ids from a list
+filter_out = lambda id, list_of_ids: not (id in list_of_ids)
 
 # we need to import python modules from the $SUMO_HOME/tools directory
 if 'SUMO_HOME' in os.environ:
@@ -39,8 +41,6 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
-
-filter_out = lambda id, list_of_ids: not (id in list_of_ids)
 
 
 def run():
@@ -70,6 +70,12 @@ def get_options():
     options, args = optParser.parse_args()
     return options
 
+def Traci_exit ():
+    traci.close()
+    sys.stdout.flush()
+    exit ()
+
+
 
 # this is the main entry point of this script
 if __name__ == "__main__":
@@ -82,18 +88,17 @@ if __name__ == "__main__":
     path_to_scenario = '../../LuSTScenario/scenario/'
 
     # this is the normal way of using traci. sumo is started as a
-    # subprocess and then the python script connects and runs
-    traci.start([sumoBinary, "-c", path_to_scenario + "due.actuated.sumocfg"]) #"--tripinfo-output", "tripinfo.xml"])
+    # subprocess and then the python script connects and runs #--no-step-log #--verbose #"'--duration-log.statistics', 'false' 
+    traci.start([sumoBinary, '-c', path_to_scenario + 'my.sumocfg', '-V', 'false', '--no-step-log', 'false']) #"--tripinfo-output", "tripinfo.xml"])
     with open('vehicles.pos', 'w') as pos_output_file:
         pos_output_file.write('')                
-    pos_output_file  = open ('vehicles.pos',  "w")
+    pos_output_file  = open ('../res/vehicles.pos',  "w")
     
-    with open('traj.pos', 'w') as traj_output_file:
-        traj_output_file.write('')                
-    traj_output_file  = open ('traj.pos',  "w")
-    
-    
-    num_of_simulated_secs = 3600
+    # with open('traj.pos', 'w') as traj_output_file:
+    #     traj_output_file.write('')                
+    # traj_output_file  = open ('traj.pos',  "w")   
+
+    num_of_simulated_secs = 585
     num_of_vehicles       = 0
     step                  = 0
     veh_key2id            = [] # will hold pairs of (veh key, veh id)
@@ -102,31 +107,39 @@ if __name__ == "__main__":
     X, Y = [], []
 
     while (step < num_of_simulated_secs and traci.simulation.getMinExpectedNumber() > 0): # There're still moving vehicles
-        printf (pos_output_file, 't = {:.2f}\n' .format (traci.simulation.getTime()))
         cur_list_of_vehicles = traci.vehicle.getIDList()
-        # list (filter (lambda veh : filter_out (veh['id'], cur_list_of_vehicles), veh_key2id)) #remove from the list all the
-        # ids2recycle = list (set (ids2recycle) | set())
+        printf (pos_output_file, 't = {:.0f}, {} active vehicles\n' .format (traci.simulation.getTime(), len(cur_list_of_vehicles)))
+        vehs_that_left = list (filter (lambda veh : veh['key'] not in (cur_list_of_vehicles), veh_key2id)) #list of cars that left the simulation
+        if (len (vehs_that_left) > 0):
+            ids2recycle = sorted (list (set (ids2recycle) | set([item['id'] for item in vehs_that_left]))) # add to ids2recycle the IDs of all cars that left
+            printf (pos_output_file, 'left vehs are: {}. IDs to recycle are {}\n' .format (vehs_that_left, ids2recycle))
         # free_ids = [veh_id for veh_id in veh_key2id]
-        # veh_key2id = list (filter (lambda veh : filter_out (veh['id'], cur_list_of_vehicles), veh_key2id)) #remove from the list all the 
+        # veh_key2id = list (filter (lambda veh : filter_out (veh['id'], cur_list_of_vehicles), veh_key2id)) #remove from the list all the
+        # printf (num_veh_output_file, '({}{:.0f}\' .format(len(cur_list_of_vehicles))) 
+        X.append (step), Y.append (len(cur_list_of_vehicles))
         for veh_key in cur_list_of_vehicles: 
-            filtered_list = list (filter (lambda veh : veh['key'] == veh_key, veh_key2id))
+            filtered_list = list (filter (lambda veh : veh['key'] == veh_key, veh_key2id)) # look for this veh in the list of already-known vehs 
             if (len(filtered_list) == 0): # first time this veh_key appears in the simulation
-                veh_id = num_of_vehicles
+                if (len (ids2recycle) > 0): # can recycle an ID of a veh that left
+                    veh_id = ids2recycle.pop(0) # recycle an ID of a veh that left, and remove it from the list of veh IDs to recycle
+                else:
+                    veh_id = num_of_vehicles # pick a new ID
+                    num_of_vehicles += 1
                 veh_key2id.append({'key' : veh_key, 'id' : veh_id}) 
-                num_of_vehicles += 1
             elif (len(filtered_list) == 1): # already seen this veh_key in the sim' --> extract its id from the hash 
                 veh_id = filtered_list[0]['id']
             elif (len(filtered_list) == 2):
                 printf (pos_output_file, 'In-naal raback\n')
-                exit ()
+                Traci_exit ()
             position = traci.vehicle.getPosition(veh_key)
             max_X_pos, max_Y_pos = max (max_X_pos, position[0]), max (max_Y_pos, position[0])
             printf (pos_output_file, "user {} \tID={}\t ({:.0f},{:.0f})\n" .format (veh_key, veh_id, position[0], position[1]))
-            X.append (position[0]), Y.append (position[1]) 
+            # X.append (position[0]), Y.append (position[1]) 
             # printf (traj_output_file, '({:.0f}, {:.0f})' .format (position[0], position[1]))
         traci.simulationStep()
         step += 1
-    plt.scatter(X, Y)
-    plt.show()
+    # plt.scatter(X, Y)
+    # plt.plot (X,Y)
+    # plt.show()
     traci.close()
     sys.stdout.flush()

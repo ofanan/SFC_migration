c801c0618940bd17f82602ffa904e95645f9e87f
ofanan <ofanan@gmail.com>
2021-07-18 12:22:02 +0300

    Started coding Res_file_parser.py functions to generate plot comparing alg and the benchmarks.
    file:///C:/Users/ofanan/Documents/GitHub/SFC_migration/src/Res_file_parser.py

--- src/Res_file_parser.py
+++ src/Res_file_parser.py
@@ -3,16 +3,52 @@ import numpy as np
 
 from printf import printf 
 
-t_idx   = 0
-alg_idx = 1
-stts_idx = 2
+t_idx    =  0
+alg_idx  =  1
+stts_idx = -1
 
 class Res_file_parser (object):  
 
     def __init__ (self):
-        print ('')
+        """
+        """
+        self.add_plot_opt   = '\t\t\\addplot [color = green, mark=+, line width = \\plotLineWidth] coordinates {\n\t\t'
+        self.add_plot_str1  = '\t\t\\addplot [color = blue, mark=square, line width = \\plotLineWidth] coordinates {\n\t\t'
+        self.add_plot_fno1  = '\t\t\\addplot [color = purple, mark=o, line width = \\plotLineWidth] coordinates {\n\t\t'
+        self.add_plot_fna1  = '\t\t\\addplot [color = red, mark=triangle*, line width = \\plotLineWidth] coordinates {\n\t\t'
+        self.add_plot_fno2  = '\t\t\\addplot [color = black, mark = square,      mark options = {mark size = 2, fill = black}, line width = \plotLineWidth] coordinates {\n\t\t'
+        self.add_plot_fna2  = '\t\t\\addplot [color = blue,  mark = *, mark options = {mark size = 2, fill = blue},  line width = \plotLineWidth] coordinates {\n\t\t'
+        self.end_add_plot_str = '\n\t\t};'
+        self.add_legend_str = '\n\t\t\\addlegendentry {'
+        self.add_plot_str_dict = {'Opt' : self.add_plot_opt, 'FNAA' : self.add_plot_fna2, 'FNOA' : self.add_plot_fno2}
+        self.legend_entry_dict = {'Opt' : '\\opt', 
+                                  'FNAA' : '\\pgmfna', 
+                                  'FNOA' : '\\pgmfno',
+                                  'FNOA' : '\\pgmfno'}
+
+    def parse_file (self, input_file_name):
+    
+        self.input_file     = open ("../res/" + input_file_name,  "r")
+        self.output_file    = open ("../res/" + input_file_name.split(".")[0] + ".dat", "w")
+        lines               = (line.rstrip() for line in self.input_file) # "lines" contains all lines in input file
+        lines               = (line for line in lines if line)       # Discard blank lines
+        self.set_of_dicts   = ()
+        
+        for line in lines:
+        
+            # Discard lines with comments / verbose data
+            if (line.split ("//")[0] == ""):
+                continue
+           
+            self.parse_line(line)
+            # if (not(self.dict in self.set_of_dicts)): # verify that such an item doesn't already exist in the list. However, if using set_of_dicts, no need for this check 
+            self.set_of_dicts.ad(self.dict)
+                
+
+        self.input_file.close
 
     def parse_line (self, line):
+
         splitted_line = line.split ("|")
          
         settings        = splitted_line[0]
@@ -27,6 +63,16 @@ class Res_file_parser (object):
             "alg"        : splitted_line        [alg_idx],   
             }
 
+    def gen_filtered_list (self, list_to_filter, alg_mode = None, alg_mode = None):
+        """
+        filters and takes from all the items in a given list (that was read from the res file) only those with the desired parameters value
+        The function filters by some parameter only if this parameter is given an input value > 0.
+        """
+        if (not (alg_mode == None)):
+            list_to_filter = list (filter (lambda item : item['alg_mode'] == alg_mode, list_to_filter))    
+        return list_to_filter
+
+    
     def plot_num_of_vehs (self):
         # Open input and output files
         input_file  = open ("../res/num_of_vehs_24_every_min_correct.pos", "r")  

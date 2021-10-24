c3b7dc77a53c16e4d3cdc9f3ecc8142151ebb40b
Itamar Cohen <ofanan@gmail.com>
2021-10-24 21:28:40 +0200

    rggr

--- src/SFC_mig_simulator.py
+++ src/SFC_mig_simulator.py
@@ -1598,3 +1598,10 @@ def run_cost_by_rsrc (poa_file_name, poa2cell_file_name):
             #         my_simulator.simulate (mode = mode, cpu_cap_at_leaf=cpu_cap_at_leaf, seed=seed)
     
 
+poa_file_name      = 'Lux_0730_0830_1secs_post.poa' #'shorter.poa' #
+poa2cell_file_name = 'Lux.post.antloc_256cells.poa2cell'
+my_simulator       = SFC_mig_simulator (poa_file_name=poa_file_name, verbose=[VERBOSE_RES], poa2cell_file_name=poa2cell_file_name)
+i = 0
+my_simulator.simulate (mode = 'opt', cpu_cap_at_leaf=int(89*(1+0.1*i)))
+# SFC_mig_simulator.run_cost_by_rsrc (poa_file_name, poa2cell_file_name)
+    

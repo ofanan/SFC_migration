e10426d7391d4ea2654aab2a9ca8f06b3184a5a7
Itamar Cohen <ofanan@gmail.com>
2021-10-26 11:14:32 +0200

    df

--- src/SFC_mig_simulator.py
+++ src/SFC_mig_simulator.py
@@ -1608,11 +1608,12 @@ poa_file_name      = 'Lux_0730_0830_16secs_post.poa' #'shorter.poa' #
 poa2cell_file_name = 'Lux.post.antloc_256cells.poa2cell'
 
 # run_cost_by_rsrc (poa_file_name, poa2cell_file_name)
-my_simulator    = SFC_mig_simulator (poa_file_name=poa_file_name, verbose=[], poa2cell_file_name=poa2cell_file_name)
+# my_simulator    = SFC_mig_simulator (poa_file_name=poa_file_name, verbose=[], poa2cell_file_name=poa2cell_file_name)
 # my_simulator.run_prob_of_RT_sim_opt  ()
-my_simulator.run_prob_of_RT_sim_algs ()
-# my_simulator       = SFC_mig_simulator (poa_file_name=poa_file_name, verbose=[VERBOSE_RES], poa2cell_file_name=poa2cell_file_name)
-# my_simulator.simulate (mode = 'ourAlg', cpu_cap_at_leaf=100)
+# my_simulator.run_prob_of_RT_sim_algs ()
+my_simulator       = SFC_mig_simulator (poa_file_name=poa_file_name, verbose=[VERBOSE_RES], poa2cell_file_name=poa2cell_file_name)
+for seed in [43 + i for i in range (17) ]:
+    my_simulator.simulate (mode = 'ourAlg', cpu_cap_at_leaf=100)
 # i = 0
 # my_simulator.simulate (mode = 'opt', cpu_cap_at_leaf=int(89*(1+0.1*i)))
     
